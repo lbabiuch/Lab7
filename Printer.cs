54ae66c0621ddbe05b634323639b7abdcbeff799
lbabiuch <lbabiuch@outlook.com>
2020-01-20 17:50:20 +0100

    poprawka (wywalenie Randoma)

--- Printer.cs
+++ Printer.cs
@@ -10,16 +10,17 @@ namespace Printer
         private int PaperCount;
         public event EventHandler OutOfPaperEvent;
         public event EventHandler<OutOfInkEventArgs> OutOfInkEvent;
+        private Random random;
         public Printer(int pages, List<Inks> inks)
         {
             PaperCount = pages;
             Inks = inks;
             OutOfPaperEvent += OutOfPaperEventHandler;
             OutOfInkEvent += OutOfInkEventHandler;
+            random = new Random();
         }
         public void Print(int pages)
         {
-            var InkUsage = new Random();
             Console.WriteLine("Printing...");
             if (PaperCount-pages < 0)
             {
@@ -32,7 +33,7 @@ namespace Printer
             {
                 foreach (var item in Inks)
                 {
-                    item.InkLeft -= Math.Min(item.InkLeft, InkUsage.Next(0, 10));
+                    item.InkLeft -= Math.Min(item.InkLeft, random.Next(0, 10));
                     if (item.InkLeft == 0)
                     {
                         OutOfInkEvent.Invoke(this,
